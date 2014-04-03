1a079878229e2a3ea13bee556dea56519d967d78
Belu Beltran <belubeltran@gmail.com>
2014-04-02 22:33:54 -0300

    Se leen las reglas de un archivo
    Rules ahora levanta las reglas del juego desde un XML.

--- 75-10-TP0-rock-paper-scissors/src/main/java/ar/fiuba/tecnicas/rockpaperscissors/Referee.java
+++ 75-10-TP0-rock-paper-scissors/src/main/java/ar/fiuba/tecnicas/rockpaperscissors/Referee.java
@@ -4,23 +4,24 @@ import java.util.HashMap;
 import java.util.Vector;
 
 public class Referee {
-
+	
 	private Rules rules = new Rules();
 	
 	/** Tabla que contiene para cada elemento, su lista de elementos a los que les gana
 	 * en una partida **/
-	private HashMap<Element,Vector<Element>> wins = new HashMap<Element,Vector<Element>>();
+	private static HashMap<Element,Vector<Element>> wins = new HashMap<Element,Vector<Element>>();
 	
 	public Referee() {
 		wins.put(Rock.getInstance(), rules.getRockWins());
 		wins.put(Paper.getInstance(), rules.getPaperWins());
 		wins.put(Scissors.getInstance(), rules.getScissorsWins());
+		System.out.println("rockWins " + rules.getRockWins().get(0).getClass().getSimpleName());
 	}
 	
 	/** El Referee decide el elemento ganador en una partida. Lo hace a partir de la
 	 * tabla wins, que construye a partir de la clase Rules. **/
 	public Element decideWinner(Element elementOne, Element elementTwo) {
-		if (wins.get(elementOne).contains(elementTwo))
+		if (wins.get(elementOne).contains(elementTwo)) 
 			return elementOne;
 		return elementTwo;
 	}
--- 75-10-TP0-rock-paper-scissors/src/main/java/ar/fiuba/tecnicas/rockpaperscissors/Rules.java
+++ 75-10-TP0-rock-paper-scissors/src/main/java/ar/fiuba/tecnicas/rockpaperscissors/Rules.java
@@ -1,17 +1,86 @@
 package ar.fiuba.tecnicas.rockpaperscissors;
+import java.io.File;
+import java.io.IOException;
 import java.util.Vector;
 
-public class Rules { 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class Rules extends DefaultHandler { 
 
 	/** Vectores que contienen los elementos a los que les gana cada uno. **/
 	private Vector<Element> rockWins = new Vector<Element>();
 	private Vector<Element> paperWins = new Vector<Element>();
 	private Vector<Element> scissorsWins = new Vector<Element>();
 	
+	/** Permiten la lectura del archivo de reglas **/
+	String tagContent = null;
+	String tagName = null;
+	  
+	/** A partir de un lector de XML, se guardan los elementos a los que le gana cada uno
+	 * en su respectivo vector de wins **/
 	private void initializeWins() {
-		rockWins.add(Scissors.getInstance());
-		paperWins.add(Rock.getInstance());
-		scissorsWins.add(Paper.getInstance());
+	    SAXParserFactory parserFactor = SAXParserFactory.newInstance();
+	    SAXParser parser;
+		try {
+			parser = parserFactor.newSAXParser();
+			parser.parse(new File("rules\\rules.xml"), this);
+		} catch (ParserConfigurationException | SAXException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/** Metodo que guarda en el respectivo vector pasado por parametros, el elemento
+	 * al que éste le gana **/
+	private void addWin(Vector<Element> wins) {
+		switch (tagContent) {
+			case "Rock":
+				wins.add(Rock.getInstance());
+				break;
+			case "Paper":
+				wins.add(Paper.getInstance());
+				break;
+			case "Scissors":
+				wins.add(Scissors.getInstance());
+				break;
+		}
+	}
+	
+	@Override
+	/** Este método es llamado cuando se encuentra un tag en el archivo XML de inicio **/
+	public void startElement(String uri, String localName, String qName, 
+			Attributes attributes) throws SAXException {
+		if (qName ==  "element") {
+			tagName = attributes.getValue("id");
+		}
+	}
+
+	@Override
+	/** Este método es llamado cuando se encuentra un tag en el archivo XML de cierre **/
+	public void endElement(String uri, String localName, String qName) 
+			throws SAXException {
+     	if (qName == "win") {
+     		if (tagName.contains("Rock")) {
+     			addWin(rockWins);
+     		}
+     		else if (tagName.contains("Paper")){
+     			addWin(paperWins);
+     		}
+     		else if (tagName.contains("Scissors")) {
+     			addWin(scissorsWins);
+     		}
+     	}
+	}
+
+	@Override
+	public void characters(char[] ch, int start, int length) 
+	        throws SAXException {
+		tagContent = String.copyValueOf(ch, start, length).trim();
 	}
 	
 	public Rules(){
